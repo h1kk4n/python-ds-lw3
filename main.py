d08d7a07e829b70f25b7a3d9426a6e409f1be010
Николай Благородов <nickblago2001@gmail.com>
2021-06-15 14:46:47 +0300

    normal default graph drawing

--- main.py
+++ main.py
@@ -12,9 +12,10 @@ topology = {}
 hosts = {}
 vulns = {}
 
+access_table = {}
 
 G = nx.DiGraph()
-nodes = set()
+nodes = []
 
 
 class InvalidIPException(Exception):
@@ -47,19 +48,19 @@ def read_files():
                     HOST_CON: [],
                     DISCON_HOST: []
                 }
-                nodes.add(current_key)
             elif line.startswith('>') and current_key:
                 router = line.replace(">", "").strip()
                 hosts[current_key][ROUTER_CON].append(validate_ip(router))
-                nodes.add(router)
             elif line.startswith('+') and current_key:
                 host = line.replace("+", "").strip()
                 hosts[current_key][HOST_CON].append(validate_ip(host))
-                nodes.add(host)
+                if host not in nodes:
+                    nodes.append(host)
             elif line.startswith("-") and current_key:
                 host = line.replace("-", "").strip()
                 hosts[current_key][DISCON_HOST].append(validate_ip(host.strip()))
-                nodes.add(host)
+                if host not in nodes:
+                    nodes.append(host)
             else:
                 current_key = ""
 
@@ -72,30 +73,48 @@ def read_files():
 def draw_graph():
     G.add_nodes_from(nodes)
 
-    for key in hosts.keys():
-        for router in hosts[key][ROUTER_CON]:
-            G.add_edge(key, router)
-            G.add_edge(router, key)
-        for host in hosts[key][HOST_CON]:
-            G.add_edge(host, key)
-            G.add_edge(key, host)
-        for host in hosts[key][DISCON_HOST]:
-            G.add_edge(host, key)
-            G.add_edge(key, host)
+    for host in access_table.keys():
+        for other_host in access_table[host]:
+            G.add_edge(host, other_host)
 
-    pos = nx.planar_layout(G)
+    pos = nx.spring_layout(G)
     plt.figure(figsize=(len(nodes) * 2, len(nodes) * 2 - 2))
     nx.draw_networkx_nodes(G, pos, node_size=2000)
-    pprint(list(G.edges)[0])
+    # pprint(list(G.edges)[0])
     nx.draw_networkx_edges(G, pos, edgelist=G.edges)
     nx.draw_networkx_labels(G, pos)
     plt.axis('off')
     plt.show()
 
 
+def create_access_table():
+    for key in hosts.keys():
+        for host in hosts[key][HOST_CON]:
+            access_table[host] = []
+            for other_host in hosts[key][HOST_CON]:
+                if other_host not in access_table[host] and other_host != host:
+                    access_table[host].append(other_host)
+            for unavailable in hosts[key][DISCON_HOST]:
+                access_table[host].append(unavailable)
+            for router in hosts[key][ROUTER_CON]:
+                for other_host in hosts[router][HOST_CON]:
+                    access_table[host].append(other_host)
+        for unavailable in hosts[key][DISCON_HOST]:
+            access_table[unavailable] = []
+            for other_host in hosts[key][HOST_CON]:
+                access_table[unavailable].append(other_host)
+            for other_unavailable in hosts[key][DISCON_HOST]:
+                if other_unavailable not in access_table[unavailable] and other_unavailable != unavailable:
+                    access_table[unavailable].append(other_unavailable)
+            for router in hosts[key][ROUTER_CON]:
+                for other_host in hosts[router][HOST_CON]:
+                    access_table[unavailable].append(other_host)
+
+
 def main():
     try:
         read_files()
+        create_access_table()
         draw_graph()
 
     except InvalidIPException as e:
@@ -104,5 +123,10 @@ def main():
 
 if __name__ == "__main__":
     main()
+    create_access_table()
+
+    pprint(access_table)
+
+    # pprint(hosts)
 
 
